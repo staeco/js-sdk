1e530c41de678c68e94f412a269bf23324ec8536
contra <yo@contra.io>
2020-08-24 12:51:24 -0700

    chore: deps

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,44 @@
-'use strict';
+"use strict";
 
 exports.__esModule = true;
-exports.createClient = undefined;
+exports.createClient = void 0;
 
-var _meta = require('./meta.json');
+var _meta = _interopRequireDefault(require("./meta.json"));
 
-var _meta2 = _interopRequireDefault(_meta);
-
-var _sutroClient = require('sutro-client');
-
-var _sutroClient2 = _interopRequireDefault(_sutroClient);
+var _sutroClient = _interopRequireDefault(require("sutro-client"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-var createClient = function createClient() {
-  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-  var key = _ref.key,
-      _ref$root = _ref.root,
-      root = _ref$root === undefined ? 'https://municipal.systems/' : _ref$root,
-      rest = _objectWithoutProperties(_ref, ['key', 'root']);
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+const createClient = (_ref = {}) => {
+  let {
+    key,
+    root = 'https://municipal.systems/'
+  } = _ref,
+      rest = _objectWithoutProperties(_ref, ["key", "root"]);
 
   if (typeof key !== 'string') throw new Error('Invalid key option!');
   if (typeof root !== 'string') throw new Error('Invalid base option!');
-  var opts = Object.assign({}, rest, {
-    root: root,
+
+  const opts = _objectSpread(_objectSpread({}, rest), {}, {
+    root,
     rewriteLargeRequests: true,
     simple: true,
-    options: Object.assign({ key: key }, rest.options)
+    options: _objectSpread({
+      key
+    }, rest.options)
   });
-  return (0, _sutroClient2.default)(_meta2.default, opts);
+
+  return (0, _sutroClient.default)(_meta.default, opts);
 };
+
 exports.createClient = createClient;
